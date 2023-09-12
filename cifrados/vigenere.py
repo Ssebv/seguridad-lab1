b48e15b263d10b3a869d73bc387f63e452eb65d1
Ssebv <sallendec@uft.edu>
2023-09-11 21:47:55 -0300

    Modificacion de carpetas y delete de algunas dependencias - pipenv

--- cifrados/vigenere.py
+++ cifrados/vigenere.py
@@ -1,11 +1,14 @@
+# Función para cifrar un mensaje utilizando el cifrado vigenere
 def vigenereEncriptar(texto, clave):
-    encrypt_text = []
+    encrypt_text = [] # Lista para almacenar caracteres cifrados
     for i in range(len(texto)):
         if 'A' <= texto[i] <= 'Z':
+            # Cifrado de letras mayúsculas
             x = (ord(texto[i]) + ord(clave[i % len(clave)]) - 2 * ord('A')) % 26
             x += ord('A')
             encrypt_text.append(chr(x))
         elif 'a' <= texto[i] <= 'z':
+            # Cifrado de letras minúsculas
             x = (ord(texto[i]) + ord(clave[i % len(clave)]) - 2 * ord('a')) % 26
             x += ord('a')
             encrypt_text.append(chr(x))
@@ -13,14 +16,17 @@ def vigenereEncriptar(texto, clave):
             encrypt_text.append(texto[i])
     return "".join(encrypt_text)
 
+# Función para descifrar un mensaje utilizando el cifrado vigenere
 def vigenereDesencriptar(texto, clave):
-    decrypt_text = []
+    decrypt_text = [] # Lista para almacenar caracteres descifrados
     for i in range(len(texto)):
         if 'A' <= texto[i] <= 'Z':
+            # Descifrado de letras mayúsculas
             x = (ord(texto[i]) - ord(clave[i % len(clave)]) + 26) % 26
             x += ord('A')
             decrypt_text.append(chr(x))
         elif 'a' <= texto[i] <= 'z':
+            # Descifrado de letras minúsculas
             x = (ord(texto[i]) - ord(clave[i % len(clave)]) + 26) % 26
             x += ord('a')
             decrypt_text.append(chr(x))
